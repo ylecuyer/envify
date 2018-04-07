ae174ae179ae1792ac7dbf1365389215eaf93434
Etienne Wan <etienne.wan@yahoo.com>
2018-04-07 17:43:40 +0200

    Enhance styles, and use Photon colors with <3

--- popup/main.js
+++ popup/main.js
@@ -2,7 +2,7 @@ let listEnvironments = document.querySelector(".list-environments")
 let btnAddEnv = document.getElementById("btn-add-env");
 
 let addEnvironment = function(url, color){
-	let environmentStr =`<input type="text" placeholder="*.dev.example.com" value="${ url || ''}"><input type="color" value="${ color || randomDefaultColor() }"><button class="btn-remove">Remove</button>`;
+	let environmentStr =`<input type="color" value="${ color || randomDefaultColor() }"><input type="text" placeholder="*.dev.example.com" value="${ url || ''}"><button class="btn-remove">Remove</button>`;
 	let environmentNode =  document.createElement("section");
 	environmentNode.classList.add("environment");
 	environmentNode.innerHTML = environmentStr;
@@ -14,7 +14,7 @@ let addEnvironment = function(url, color){
 }
 
 let randomDefaultColor = function(){
-	let flatColors = [ "#1abc9c","#2ecc71","#3498db","#9b59b6","#34495e","#16a085","#27ae60","#2980b9","#8e44ad","#2c3e50","#f1c40f","#e67e22","#e74c3c","#ecf0f1","#95a5a6","#f39c12","#d35400","#c0392b","#bdc3c7","#7f8c8d"];
+	let flatColors = [ "#0a84ff", "#00feff", "#ff1ad9", "#30e60b", "#ffe900", "#ff0039", "#9400ff", "#ff9400", "#363959", "#737373"];
 	return flatColors[ Math.floor(Math.random() * flatColors.length)]
 }
 
