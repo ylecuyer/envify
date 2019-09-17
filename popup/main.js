14fe212c650c9feea8b34c992de34980dec31e31
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2019-09-17 23:44:14 +0200

    export

--- popup/main.js
+++ popup/main.js
@@ -66,3 +66,24 @@ document.addEventListener("DOMContentLoaded", function(){
 		btnAddEnv.reportValidity();
 	})
 }, false)
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+document.getElementById('export').addEventListener('click', function() {
+  browser.storage.sync.get("environments")
+	.then(function(env) {
+    download('envify.json', JSON.stringify(env))
+  })
+
+});
